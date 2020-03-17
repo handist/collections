3db70f26e62336e74e6bcc8539bc8d13431e3ffb
Tomio Kamada <kamada@fine.cs.kobe-u.ac.jp>
2020-03-17 16:17:57 +0900

    revert? launcher

--- src/handist/util/mpi/ApgasMPILauncher.java
+++ src/handist/util/mpi/ApgasMPILauncher.java
@@ -9,6 +9,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 import apgas.Configuration;
@@ -209,49 +210,62 @@ final public class ApgasMPILauncher implements Launcher {
    */
   // }
 
-    public static void main(String[] args) throws Exception {
-
-        if (args.length < 1) {
-            // TODO refine error message after preparing launch script.
-            System.err.println("[ApgasMPILauncher] Error Main Class Required.");
-            System.exit(0);
-        }
-
-        MPI.Init(args);
-        commRank = MPI.COMM_WORLD.Rank();
-        commSize = MPI.COMM_WORLD.Size();
-
-        System.err.println("[ApgasMPILauncher] rank = " + commRank);
-
-        System.setProperty(Configuration.APGAS_PLACES, Integer.toString(commSize));
-        System.setProperty(Config.APGAS_LAUNCHER, "handist.util.mpi.ApgasMPILauncher");
-        String[] restArgs = new String[args.length-1];
-        System.arraycopy(args, 1, restArgs, 0, args.length-1);
-
-        if (commRank == 0) {
-
-            TeamedPlaceGroup.worldSetup();
-            try {
-                final Method mainMethod = Class.forName(restArgs[0]).getMethod("main",
-                        String[].class);
-                final Object[] mainArgs = new Object[1];
-                mainArgs[0] = restArgs;
-                mainMethod.invoke(null, mainArgs);
-                MPI.Finalize();
-                System.exit(0);
-            } catch (final Exception e) {
-                e.printStackTrace();
-                MPI.Finalize();
-                System.exit(0);
-            }
+  public static void main(String[] args) throws Exception {
+    ArrayList<String> bag = new ArrayList<>();
+    String[] mpiArgs = new String[0];
+    String[] restArgs = new String[0];
+    for(int i = 0; i<args.length; i++) {
+        if(args[i].equals("-body")) {
+            mpiArgs = bag.toArray(mpiArgs);
+            bag.clear();
         } else {
-            slave();
-            System.err.println("[ApgasMPILauncher] rank = " + commRank + ", here" + here());
-            TeamedPlaceGroup.worldSetup();
+            bag.add(args[i]);
         }
+    }
+    restArgs = bag.toArray(restArgs);
+    System.err.println("headArgs:"+ java.util.Arrays.toString(mpiArgs));
+    System.err.println("restArgs:"+java.util.Arrays.toString(restArgs));
 
+    if (mpiArgs.length==0 || restArgs.length==0) {
+        // TODO refine error message after preparing launch script.
+        System.err.println("[ApgasMPILauncher] Error Main Class Required.");
         MPI.Finalize();
         System.exit(0);
+      }
+
+    MPI.Init(mpiArgs);
+    commRank = MPI.COMM_WORLD.Rank();
+    commSize = MPI.COMM_WORLD.Size();
+
+    System.err.println("[ApgasMPILauncher] rank = " + commRank);
+
+    System.setProperty(Configuration.APGAS_PLACES, Integer.toString(commSize));
+    System.setProperty(Config.APGAS_LAUNCHER, "handist.util.mpi.ApgasMPILauncher");
+
+    if (commRank == 0) {
+
+    TeamedPlaceGroup.worldSetup();
+      try {
+        final Method mainMethod = Class.forName(restArgs[0]).getMethod("main",
+            String[].class);
+        final Object[] mainArgs = new Object[1];
+        mainArgs[0] = restArgs;
+        mainMethod.invoke(null, mainArgs);
+        MPI.Finalize();
+        System.exit(0);
+      } catch (final Exception e) {
+        e.printStackTrace();
+        MPI.Finalize();
+        System.exit(0);
+      }
+    } else {
+      slave();
+    System.err.println("[ApgasMPILauncher] rank = " + commRank + ", here" + here());
+    TeamedPlaceGroup.worldSetup();
     }
+
+    MPI.Finalize();
+    System.exit(0);
+  }
 
 }
