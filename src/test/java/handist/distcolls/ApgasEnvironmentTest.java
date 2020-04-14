6d5c112d3c175549069258743d75f7305d66e78f
patrick.finnerty <patrick.finnerty@insa-lyon.fr>
2020-04-14 10:54:35 +0900

    Programmed an example of how to handle a failed assertion in a remote asynchronous activity in the Junit tests.

--- src/test/java/handist/distcolls/ApgasEnvironmentTest.java
+++ src/test/java/handist/distcolls/ApgasEnvironmentTest.java
@@ -7,9 +7,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import apgas.GlobalRuntime;
+import apgas.MultipleException;
 
 import static apgas.Constructs.*;
 
+/**
+ * Test class that checks how the Junit various asserts behave with the APGAS
+ * remote activities and illustrates how to handle them. 
+ */
 public class ApgasEnvironmentTest {
 
 	@BeforeClass
@@ -22,19 +27,31 @@ public class ApgasEnvironmentTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * Checks that the test was indeed run with multiple places
+	 */
 	@Test
 	public void testMultipleHosts() {
 		int numberOfPlaces = places().size();
 		assertTrue("This test should be run with multiple hosts", numberOfPlaces > 1);
 	}
 
-	@Test()
-	public void testRemoteFail() {
-		finish(()-> {
-			final int rootId = here().id;
-			asyncAt(place(1), () -> {
-				assertEquals(rootId, here().id); // Expect failure
+	/**
+	 * Tests the failure of an assertEquals call in an asynchronous remote
+	 * remote activity.
+	 */
+	@Test(expected=java.lang.AssertionError.class)
+	public void testRemoteAssertEqualsFailure() throws Throwable {
+		try {
+			finish(()-> {
+				final int rootId = here().id;
+				asyncAt(place(1), () -> {
+					assertEquals(rootId, here().id); // Expect failure
+				});
 			});
-		});
+		} catch (MultipleException e) {
+			Throwable[] suppressed = e.getSuppressed();
+			throw(suppressed[0]);
+		}
 	}
 }
