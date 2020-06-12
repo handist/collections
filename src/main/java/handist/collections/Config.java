8791f3213c4ead86aef050f3aa8e59e146edd87b
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2020-06-12 16:19:08 +0900

    Change Config omitTostring

--- src/main/java/handist/collections/Config.java
+++ src/main/java/handist/collections/Config.java
@@ -2,7 +2,7 @@ package handist.collections;
 
 public interface Config {
     int maxNumElementsToString = 10; // 10 is default
-    boolean omitElementsToString = false; // true is default
+    boolean omitElementsToString = true; // true is default
     long maxChunkSize = Integer.MAX_VALUE;
 }
 
