72a2b56665521f4253cef60fcbfe549846e29632
Tomio Kamada <kamada@fine.cs.kobe-u.ac.jp>
2020-06-15 21:17:57 +0900

    untabify futureN

--- src/main/java/handist/collections/FutureN.java
+++ src/main/java/handist/collections/FutureN.java
@@ -9,67 +9,67 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
 public class FutureN<S>  {
-    protected final List<Future<S>> futures;
-    boolean isCanceled = false;
-    boolean isDone = false;
+	protected final List<Future<S>> futures;
+	boolean isCanceled = false;
+	boolean isDone = false;
 
-    public FutureN(List<Future<S>> futures) {
-        this.futures = futures;
-    }
+	public FutureN(List<Future<S>> futures) {
+		this.futures = futures;
+	}
 
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        boolean result = true;
-        for (Future<?> f : futures)
-            result &= f.cancel(mayInterruptIfRunning);
-        return result;
-    }
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		boolean result = true;
+		for (Future<?> f : futures)
+			result &= f.cancel(mayInterruptIfRunning);
+		return result;
+	}
 
-    public boolean isCancelled() {
-        boolean result = true;
-        for (Future<S> f : futures)
-            result &= f.isCancelled();
-        return result;
-    }
+	public boolean isCancelled() {
+		boolean result = true;
+		for (Future<S> f : futures)
+			result &= f.isCancelled();
+		return result;
+	}
 
-    public boolean isDone() {
-        synchronized (this) {
-            if (isDone)
-                return isDone;
-        }
-        for (Future<S> f : futures) {
-            if (!f.isDone())
-                return false;
-        }
-        synchronized (this) {
-            isDone = true;
-        }
-        return true;
-    }
+	public boolean isDone() {
+		synchronized (this) {
+			if (isDone)
+				return isDone;
+		}
+		for (Future<S> f : futures) {
+			if (!f.isDone())
+				return false;
+		}
+		synchronized (this) {
+			isDone = true;
+		}
+		return true;
+	}
 
-    public void get0() throws InterruptedException, ExecutionException {
-        synchronized (this) {
-            if (isDone) return;
-        }
-        for (Future<?> f : futures)
-            f.get();
-        return;
-    }
+	public void get0() throws InterruptedException, ExecutionException {
+		synchronized (this) {
+			if (isDone) return;
+		}
+		for (Future<?> f : futures)
+			f.get();
+		return;
+	}
 
-    public void get0(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        long start = System.currentTimeMillis();
-        synchronized (this) {
-            if (isDone)
-                return;
-        }
-        for (Future<?> f : futures) {
-            long now = System.currentTimeMillis();
-            f.get(timeout - unit.convert(now - start, TimeUnit.MILLISECONDS), unit);
-        }
-        return;
-    }
+	public void get0(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		long start = System.currentTimeMillis();
+		synchronized (this) {
+			if (isDone)
+				return;
+		}
+		for (Future<?> f : futures) {
+			long now = System.currentTimeMillis();
+			f.get(timeout - unit.convert(now - start, TimeUnit.MILLISECONDS), unit);
+		}
+		return;
+	}
 
-    public static class ReturnGivenResult<R> extends FutureN implements Future<R> {
-    	R result;
+	public static class ReturnGivenResult<R> extends FutureN implements Future<R> {
+		R result;
 		public ReturnGivenResult(List<Future<?>> futures, R result) {
 			super(futures);
 		}
@@ -85,13 +85,13 @@ public class FutureN<S>  {
 			get0(timeout, unit);
 			return result;
 		}
-    }
-    public static class OnlyWait extends ReturnGivenResult<Void> {
+	}
+	public static class OnlyWait extends ReturnGivenResult<Void> {
 		public OnlyWait(List<Future<?>> futures) {
 			super(futures, null);
 		}
-    }
-    public static class ListResults<R> extends FutureN<R> implements Future<List<R>> {
+	}
+	public static class ListResults<R> extends FutureN<R> implements Future<List<R>> {
 		public ListResults(List<Future<R>> futures) {
 			super(futures);
 		}
@@ -121,10 +121,10 @@ public class FutureN<S>  {
 			}
 			return result;
 		}
-    }
-    public static class ConsumeResults<R> extends FutureN<R> implements Future<Void> {
+	}
+	public static class ConsumeResults<R> extends FutureN<R> implements Future<Void> {
 
-    	private Consumer<R> consumer;
+		private Consumer<R> consumer;
 		public ConsumeResults(List<Future<R>> futures, Consumer<R> consumer) {
 			super(futures);
 			this.consumer = consumer;
@@ -156,5 +156,5 @@ public class FutureN<S>  {
 				processed = true;
 			}
 		}
-    }
+	}
 }
