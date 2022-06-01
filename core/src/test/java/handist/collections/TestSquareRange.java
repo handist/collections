a6c599b9238f299991e1bec9e11453b1394f24b8
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2022-06-01 17:29:20 +0900

    Fixed SquareRange intersection and size

--- core/src/test/java/handist/collections/TestSquareRange.java
+++ core/src/test/java/handist/collections/TestSquareRange.java
@@ -13,25 +13,38 @@ public class TestSquareRange {
     private final LongRange outerRange = new LongRange(1, 10);
     private final LongRange innerRange = new LongRange(2, 10);
 
-    private SquareRange square;
+    private final SquareRange square = new SquareRange(outerRange, innerRange);
     /**
-     * &ensp;&nbsp;123456789<br>
-     * 1 011111111<br>
-     * 2 001111111<br>
-     * 3 000111111<br>
-     * 4 000011111<br>
-     * 5 000001111<br>
-     * 6 000000111<br>
-     * 7 000000011<br>
-     * 8 000000001<br>
-     * 9 000000000<br>
+     * &ensp;&nbsp;23456789<br>
+     * 1 11111111<br>
+     * 2 01111111<br>
+     * 3 00111111<br>
+     * 4 00011111<br>
+     * 5 00001111<br>
+     * 6 00000111<br>
+     * 7 00000011<br>
+     * 8 00000001<br>
+     * 9 00000000<br>
      */
-    private SquareRange upperTriangle;
+    private final SquareRange upperTriangle = new SquareRange(outerRange, innerRange, true);
+
+    /**
+     * &ensp;&nbsp;01<br>
+     * 8 00<br>
+     * 9 00<br>
+     */
+    private final SquareRange upperTriangle_empty = new SquareRange(new LongRange(8, 10), new LongRange(0, 2), true);
+
+    private final SquareRange topLeft = new SquareRange(new LongRange(0, 2), new LongRange(1, 3));
+    private final SquareRange topRight = new SquareRange(new LongRange(0, 2), new LongRange(9, 11));
+    private final SquareRange bottomLeft = new SquareRange(new LongRange(9, 11), new LongRange(1, 3));
+    private final SquareRange bottomRight = new SquareRange(new LongRange(9, 11), new LongRange(9, 11));
+    private final SquareRange center = new SquareRange(new LongRange(4, 6), new LongRange(4, 6));
+    private final SquareRange large = new SquareRange(new LongRange(0, 11), new LongRange(1, 11));
 
     @Before
     public void setup() {
-        square = new SquareRange(outerRange, innerRange);
-        upperTriangle = new SquareRange(outerRange, innerRange, true);
+
     }
 
     @After
@@ -46,6 +59,10 @@ public class TestSquareRange {
             assertEquals(innerRange, square.columnRange(row));
             assertEquals(new LongRange(innerRange.from + i, innerRange.to), upperTriangle.columnRange(row));
             i++;
+        }
+        // check uppertriangle empty
+        for (long row = 8; row < 10; row++) {
+            assertEquals(new LongRange(2, 2), upperTriangle_empty.columnRange(row));
         }
     }
 
@@ -72,11 +89,21 @@ public class TestSquareRange {
     @Test
     public void testContainsWithRange() {
         assertTrue(square.contains(new SquareRange(outerRange, innerRange)));
-        assertTrue(square.contains(new SquareRange(new LongRange(2, 9), new LongRange(3, 9))));
-        assertFalse(square.contains(new SquareRange(new LongRange(-1, 9), new LongRange(3, 9))));
-        assertFalse(square.contains(new SquareRange(new LongRange(2, 9), new LongRange(-1, 9))));
-        assertFalse(square.contains(new SquareRange(new LongRange(-5, -1), new LongRange(-5, -1))));
-        assertFalse(square.contains(new SquareRange(new LongRange(10, 15), new LongRange(10, 15))));
+        assertTrue(square.contains(center));
+        assertFalse(square.contains(topLeft));
+        assertFalse(square.contains(topRight));
+        assertFalse(square.contains(bottomLeft));
+        assertFalse(square.contains(bottomRight));
+        assertFalse(square.contains(large));
+
+        assertTrue(square.contains(new SquareRange(outerRange, innerRange, true)));
+        assertTrue(square.contains(new SquareRange(center.outer, center.inner, true)));
+        assertFalse(upperTriangle.contains(center));
+        assertFalse(upperTriangle.contains(topLeft));
+        assertFalse(upperTriangle.contains(topRight));
+        assertFalse(upperTriangle.contains(bottomLeft));
+        assertFalse(upperTriangle.contains(bottomRight));
+        assertFalse(upperTriangle.contains(large));
     }
 
     @Test
@@ -91,28 +118,35 @@ public class TestSquareRange {
     public void testIntersection() {
         assertEquals(new SquareRange(new LongRange(1, 10), new LongRange(2, 10), true),
                 square.intersection(upperTriangle));
-        // check upperTriangle
-        assertEquals(new SquareRange(new LongRange(3, 7), new LongRange(3, 7), true),
-                upperTriangle.intersection(new SquareRange(new LongRange(3, 7), new LongRange(3, 7), true, 2)));
+        assertEquals(new SquareRange(new LongRange(1, 10), new LongRange(2, 10), true),
+                upperTriangle.intersection(square));
+        // check upperTriangles with triangle diff
+        assertEquals(new SquareRange(new LongRange(4, 6), new LongRange(4, 6), true),
+                upperTriangle.intersection(new SquareRange(new LongRange(4, 6), new LongRange(4, 6), true, 2)));
+        // check intersections of the upper triangle and square (in the lower triangle).
+        assertNull(upperTriangle.intersection(bottomLeft));
+        // check intersections of upper triangle and no intersections at all.
+        assertNull(upperTriangle.intersection(new SquareRange(new LongRange(10, 11), new LongRange(0, 2))));
     }
 
     @Test
     public void testIsOverlapped() {
         // square with square
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(1, 10), new LongRange(2, 10))));
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(2, 9), new LongRange(3, 9))));
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(-10, 2), new LongRange(3, 9))));
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(2, 9), new LongRange(9, 20))));
-        assertFalse(square.isOverlapped(new SquareRange(new LongRange(-10, 1), new LongRange(3, 9))));
-        assertFalse(square.isOverlapped(new SquareRange(new LongRange(2, 9), new LongRange(10, 20))));
+        assertTrue(square.isOverlapped(square));
+        assertTrue(square.isOverlapped(center));
+        assertTrue(square.isOverlapped(topLeft));
+        assertTrue(square.isOverlapped(topRight));
+        assertTrue(square.isOverlapped(bottomLeft));
+        assertTrue(square.isOverlapped(bottomRight));
         // square with triangle
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(1, 3), new LongRange(9, 11), true)));
-        assertTrue(square.isOverlapped(new SquareRange(new LongRange(0, 2), new LongRange(8, 10), true)));
-        // triangle with square
-        assertTrue(upperTriangle.isOverlapped(new SquareRange(new LongRange(1, 10), new LongRange(2, 10))));
-        assertTrue(upperTriangle.isOverlapped(new SquareRange(new LongRange(3, 5), new LongRange(3, 5))));
-        assertFalse(upperTriangle.isOverlapped(new SquareRange(new LongRange(4, 6), new LongRange(3, 5))));
-        assertFalse(upperTriangle.isOverlapped(new SquareRange(new LongRange(3, 5), new LongRange(2, 4))));
+        assertTrue(upperTriangle.isOverlapped(center));
+        assertTrue(upperTriangle.isOverlapped(topLeft));
+        assertTrue(upperTriangle.isOverlapped(topRight));
+        assertFalse(upperTriangle.isOverlapped(bottomLeft));
+        assertTrue(upperTriangle.isOverlapped(bottomRight));
+        // square with triangle (with triangle diff)
+        assertTrue(square.isOverlapped(new SquareRange(new LongRange(1, 5), new LongRange(2, 5), true, 1)));
+        assertFalse(square.isOverlapped(new SquareRange(new LongRange(1, 5), new LongRange(2, 5), true, -10)));
     }
 
     @Test
@@ -124,14 +158,21 @@ public class TestSquareRange {
             assertEquals(new LongRange(outerRange.from, outerRange.from + i), upperTriangle.rowRange(column));
             i++;
         }
+        // check uppertriangle empty
+        for (long column = 0; column < 2; column++) {
+            assertEquals(new LongRange(8, 8), upperTriangle_empty.rowRange(column));
+        }
     }
 
     @Test
     public void testSize() {
         assertEquals(outerRange.size() * innerRange.size(), square.size());
         assertEquals(36, upperTriangle.size());
-        assertEquals(199, new SquareRange(new LongRange(0, 2), new LongRange(0, 100), true).size());
-        assertEquals(3, new SquareRange(new LongRange(0, 100), new LongRange(0, 2), true).size());
+        assertEquals(0, upperTriangle_empty.size());
+        assertEquals(197, new SquareRange(new LongRange(0, 2), new LongRange(0, 100), true).size());
+        assertEquals(1, new SquareRange(new LongRange(0, 100), new LongRange(0, 2), true).size());
+        assertEquals(199, new SquareRange(new LongRange(0, 2), new LongRange(0, 100), true, 1).size());
+        assertEquals(3, new SquareRange(new LongRange(0, 100), new LongRange(0, 2), true, 1).size());
     }
 
     @Test
